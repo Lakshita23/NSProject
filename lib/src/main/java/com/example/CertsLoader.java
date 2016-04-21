0be27f663414d2ecb88e077014db4d2f438e4602
Lakshita23 <lakshita_lak95@yahoo.com>
2016-04-21 17:51:55 +0800

    commit

--- lib/src/main/java/com/example/CertsLoader.java
+++ lib/src/main/java/com/example/CertsLoader.java
@@ -4,8 +4,8 @@ package com.example;
  * Created by Lakshita on 4/21/2016.
  */
 public class CertsLoader {
-    public static final String SERVER_PRIVATE_KEY = "NSProject\\certificates\\privateServer.der";
-    public static final String SERVER_PUBLIC_KEY = "NSProject\\certificates\\publicServer.der";
+    public static final String SERVER_PRIVATE_KEY = "certificates\\privateServer.der";
+    public static final String SERVER_PUBLIC_KEY = "certificates\\publicServer.der";
     public static final String CA_CERTIFICATE = "certificates\\CA.crt";
     public static final String SERVER_CERTIFICATE = "certificates\\Signed Certificate - 1000943.crt";
 }
